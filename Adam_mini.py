ee5fb3acb7d7ffac2d1dcf08775953558b746619
zyushun <yushunzhang@link.cuhk.edu.cn>
2024-06-30 17:00:04 +0800

    0630update

--- Adam_mini.py
+++ Adam_mini.py
@@ -9,30 +9,29 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class Adam_mini(Optimizer):
     def __init__(
-        self,
-        model=None,
-        weight_decay=0.1,
-        lr=1,
-        beta1=0.9,
-        beta2=0.999,
-        epsilon=1e-8,
-        zero_3=False,
-        n_embd = 2048,
-        n_head = 32,
-        n_query_groups = None
+            self,
+            model=None,
+            weight_decay=0.1,
+            lr=1,
+            beta1=0.9,
+            beta2=0.999,
+            epsilon=1e-8,
+            zero_3=False,
+            n_embd=2048,
+            n_head=32,
+            n_query_groups=None
     ):
         '''
         model: the model you are training.
 
         zero_3: set to True if you are using zero_3 in Deepspeed, or if you are using model parallelism with more than 1 GPU. Set to False if otherwise.
-        
+
         n_embd: number of embedding dimensions. Could be unspecified if you are training non-transformer models.
-        
+
         n_head: number of attention heads. Could be unspecified if you are training non-transformer models.
-        
+
         n_query_groups: number of query groups in Group query Attention. If not specified, it will be equal to n_head. Could be unspecified if you are training non-transformer models.
         '''
-       
 
         self.n_embd = n_embd
         self.n_head = n_head
@@ -58,10 +57,10 @@ class Adam_mini(Optimizer):
                     dic["weight_decay"] = 0
                 else:
                     dic["weight_decay"] = weight_decay
-                
-                if ("self_attn.k_proj.weight" in name or "self_attn.q_proj.weight" in name):
+
+                if ("self_attn.k_proj.weight" in name or "self_attn.q_proj.weight" in name or "attn.wq.weight" in name or "attn.wk.weight"):
                     dic["parameter_per_head"] = self.n_embd * self.n_embd // self.n_head
-                
+
                 if ("attn.attn.weight" in name or "attn.qkv.weight" in name):
                     dic["n_head"] = self.n_head
                     dic["q_per_kv"] = self.n_head // self.n_query_groups
@@ -72,7 +71,6 @@ class Adam_mini(Optimizer):
 
         super(Adam_mini, self).__init__(optim_groups, defaults)
 
-
     def step(self):
         with torch.no_grad():
             for group in self.param_groups:
@@ -81,7 +79,7 @@ class Adam_mini(Optimizer):
                 lr = group["lr"]
                 name = group["name"]
                 epsilon = group["epsilon"]
-                
+
                 for p in group["params"]:
                     state = self.state[p]
                     if ("embed_tokens" in name or "wte" in name or "lm_head" in name):
@@ -105,15 +103,16 @@ class Adam_mini(Optimizer):
                         bias_correction_2_sqrt = math.sqrt(bias_correction_2)
 
                         h = (state["v"].sqrt() / bias_correction_2_sqrt).add_(epsilon)
-                        stepsize = lr/ bias_correction_1
+                        stepsize = lr / bias_correction_1
                         p.addcdiv_(state["m"], h, value=-stepsize)
 
-                    elif ("self_attn.k_proj.weight" in name or "self_attn.q_proj.weight" in name or "attn.wq.weight" in name or "attn.wk.weight" in name):
+                    elif (
+                            "self_attn.k_proj.weight" in name or "self_attn.q_proj.weight" in name or "attn.wq.weight" in name or "attn.wk.weight" in name):
                         if p.grad is None:
                             continue
                         dim = group["parameter_per_head"]
-                        if (len(state)==0):
-                            state["m"]  =  torch.zeros_like(p.data).to(torch.float32)
+                        if (len(state) == 0):
+                            state["m"] = torch.zeros_like(p.data).to(torch.float32)
                             state["m"] = state["m"].view(-1, dim)
                             state['head'] = state['m'].shape[0]
                             state["iteration"] = 0
@@ -123,7 +122,7 @@ class Adam_mini(Optimizer):
                         head = state['head']
                         grad = grad.view(head, dim)
 
-                        tmp_lr = torch.mean(grad*grad, dim = 1).to(device)
+                        tmp_lr = torch.mean(grad * grad, dim=1).to(device)
                         state["vmean"].mul_(beta2).add_(tmp_lr, alpha=1 - beta2)
                         v = state["vmean"]
 
@@ -137,66 +136,65 @@ class Adam_mini(Optimizer):
                         bias_correction_2 = 1 - beta2 ** state["iteration"]
                         bias_correction_2_sqrt = math.sqrt(bias_correction_2)
 
-                        h = (v.sqrt() / bias_correction_2_sqrt).add_(epsilon)    
-                        stepsize = ((1/bias_correction_1) / h).view(head,1)
+                        h = (v.sqrt() / bias_correction_2_sqrt).add_(epsilon)
+                        stepsize = ((1 / bias_correction_1) / h).view(head, 1)
 
                         update = state["m"] * (stepsize.to(state['m'].device))
 
                         if p.dim() > 1:
                             d0, d1 = p.size()
                             update = update.view(d0, d1)
-                        else: 
+                        else:
                             update = update.view(-1)
 
                         update.mul_(lr)
                         p.add_(-update)
-                        
-                    elif ("attn.attn.weight" in name or "attn.qkv.weight" in name): 
+
+                    elif ("attn.attn.weight" in name or "attn.qkv.weight" in name):
                         if p.grad is None:
                             continue
-                        if (len(state)==0):
-                            state["m"]  =  torch.zeros_like(p.data).to(torch.float32)
+                        if (len(state) == 0):
+                            state["m"] = torch.zeros_like(p.data).to(torch.float32)
                             state["m"] = state["m"].view(group["n_head"], group["q_per_kv"] + 2, -1)
                             state["iteration"] = 0
-                            state["vmean"] = torch.zeros(group["n_head"], group["q_per_kv"]+2).to(device)
-                            
+                            state["vmean"] = torch.zeros(group["n_head"], group["q_per_kv"] + 2).to(device)
 
                         grad = p.grad.data.to(torch.float32)
-                        grad = grad.view(group["n_head"], group["q_per_kv"] + 2, -1) 
+                        grad = grad.view(group["n_head"], group["q_per_kv"] + 2, -1)
 
-                        tmp_lr = torch.mean(grad*grad, dim = 2).to(device)
+                        tmp_lr = torch.mean(grad * grad, dim=2).to(device)
                         state["vmean"].mul_(beta2).add_(tmp_lr, alpha=1 - beta2)
                         v = state["vmean"]
-                
+
+                        print(f'name = {name} tmp_lr = {tmp_lr.size()}, vmean = {v.size()}')
+
                         state["iteration"] += 1
                         if group["weight_decay"] != 0:
                             p.data.mul_(1 - lr * group["weight_decay"])
 
                         state["m"].lerp_(grad, 1 - beta1)
 
-
                         bias_correction_1 = 1 - beta1 ** state["iteration"]
                         bias_correction_2 = 1 - beta2 ** state["iteration"]
                         bias_correction_2_sqrt = math.sqrt(bias_correction_2)
 
-                        h = (v.sqrt() / bias_correction_2_sqrt).add_(epsilon)    
-                        stepsize = ((1/bias_correction_1) / h).view(group["n_head"],group["q_per_kv"]+2,1)
-                                                    
-   
+                        h = (v.sqrt() / bias_correction_2_sqrt).add_(epsilon)
+                        stepsize = ((1 / bias_correction_1) / h).view(group["n_head"], group["q_per_kv"] + 2, 1)
+
                         update = state["m"] * (stepsize.to(state['m'].device))
-            
+
                         if p.dim() > 1:
                             d0, d1 = p.size()
                             update = update.view(d0, d1)
-                        else: 
+                        else:
                             update = update.view(-1)
-        
+
                         update.mul_(lr)
                         p.add_(-update)
 
-                        
-                    else:        
-                        if (len(state)==0):                   
+
+                    else:
+                        if (len(state) == 0):
                             dimension = torch.tensor(p.data.numel()).to(device).to(torch.float32)
                             reduced = False
                             if (self.world_size > 1) and (self.zero_optimization_stage == 3):
@@ -205,29 +203,29 @@ class Adam_mini(Optimizer):
                                 s = 0
                                 dimension = 0
                                 for d in tensor_list:
-                                    if (d>0):
+                                    if (d > 0):
                                         s = s + 1
                                     dimension = dimension + d
-                                if (s>=2):
+                                if (s >= 2):
                                     reduced = True
-                            
+
                             state["m"] = torch.zeros_like(p.data).to(torch.float32)
                             state["iteration"] = 0
                             state["reduced"] = reduced
-                            state["vmean"] = torch.tensor(0.0).to(device)                                
+                            state["vmean"] = torch.tensor(0.0).to(device)
                             state["dimension"] = dimension.item()
                         if p.grad is None:
                             tmp_lr = torch.tensor(0.0).to(device)
                         else:
                             grad = p.grad.data.to(torch.float32)
-                            tmp_lr = torch.sum(grad*grad).to(device)                              
+                            tmp_lr = torch.sum(grad * grad).to(device)
                         if (state["reduced"]):
                             dist.all_reduce(tmp_lr, op=dist.ReduceOp.SUM)
                         if (p.grad is None):
                             continue
                         tmp_lr = tmp_lr / (state["dimension"])
                         tmp_lr = tmp_lr.to(grad.device)
-                        
+
                         if group["weight_decay"] != 0:
                             p.data.mul_(1 - lr * group["weight_decay"])
                         state["iteration"] += 1
@@ -237,11 +235,11 @@ class Adam_mini(Optimizer):
                         bias_correction_2 = 1 - beta2 ** state["iteration"]
                         bias_correction_2_sqrt = math.sqrt(bias_correction_2)
                         state["vmean"] = (1 - beta2) * tmp_lr + beta2 * state["vmean"]
-                        h = (state["vmean"].sqrt() / bias_correction_2_sqrt).add_(epsilon)    
+                        h = (state["vmean"].sqrt() / bias_correction_2_sqrt).add_(epsilon)
 
                         stepsize = (1 / bias_correction_1) / h
                         update = state["m"] * (stepsize.to(state['m'].device))
                         update.mul_(lr)
-                        p.add_(-update)    
-                    
-     
+                        p.add_(-update)
+
+
